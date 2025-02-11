b4108b584580dc8cface4d4ba4a7c4f2f962b45d
KON THAINA <143169514+Konthaina@users.noreply.github.com>
2025-02-11 19:11:35 +0700

    Initial commit

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,23 +1,14 @@
 import TicTacToe from "../../assets/tic-tac-toe.png";
-import AirBooking from "../../assets/air booking.jpg";
-import AirLanding from "../../assets/air Landing.jpg";
-import SocialMediaAcc from "../../assets/social media account.jpg";
-import BookWorm from "../../assets/book worm.jpg";
-import ChatApp from "../../assets/chat application.jpg";
 import CoffeeShop from "../../assets/Edencoffee.png";
-import CropMonitoring from "../../assets/crop monitoring.jpg";
-import YumYard from "../../assets/yum yard.jpg";
 import Portfolio from "../../assets/portfolio.png";
-import CeylonView from "../../assets/ceylon view.jpg";
-import WordCounter from "../../assets/word counter.jpg";
-
+import Schoolms from "../../assets/schoolms.png";
 export const projectsData = [
     {
         id: 1,
         image: TicTacToe,
         title: 'Project X - Tic Tac Toe Game',
         category: 'web',
-        link: 'N/A',
+        link: 'https://github.com/Konthaina/tic-tac-toe-game',
     },
     {
         id: 2,
@@ -28,74 +19,19 @@ export const projectsData = [
     },
     {
         id: 3,
-        image: CeylonView,
-        title: 'Ceylon View - Travel site ',
-        category: 'web',
-        link: 'N/A',    
-    },
-    {
-        id: 4,
-        image: WordCounter,
-        title: 'Word Counter',
-        category: 'web',
-        link: 'N/A',
-    },
-    {
-        id: 7,
         image: CoffeeShop,
         title: 'Eden Coffee POS System',
         category: 'web',
         link:'https://github.com/Konthaina/Eden-Coffee-Frontend',
     },
     {
-        id: 5,
-        image: CropMonitoring,
-        title: 'Crop Monitoring POS System',
-        category: 'app',
-        link: 'N/A',
-    },
-    {
-        id: 8,
+        id: 4,
         image: BookWorm,
-        title: 'Library Management System',
-        category: 'app',
-        link: 'N/A',
+        title: 'School Management System',
+        category: 'web',
+        link: 'https://github.com/Konthaina/School-management',
     },
-    {
-        id: 9,
-        image: ChatApp,
-        title: 'Chit Chat - Chat Application',
-        category: 'app',
-        link: 'N/A',
-    },
-    {
-        id: 10,
-        image: TicTacToe,
-        title: 'Project X - Tic Tac Toe Game',  
-        category: 'designs',
-        link: 'N/A',
-    },
-    {
-        id: 12,
-        image: AirLanding,
-        title: 'Air Landing Page',
-        category: 'designs',
-        link: 'N/A',
-    },
-    {
-        id: 11,
-        image: AirBooking,
-        title: 'Air Bookin App',
-        category: 'designs',
-        link: 'N/A',
-    },
-    {
-        id: 13,
-        image: SocialMediaAcc,
-        title: 'Social Media Account',
-        category: 'designs',
-        link: 'N/A',
-    },
+    
 ];
 
 export const projectsNav = [
